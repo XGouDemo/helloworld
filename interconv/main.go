7b5cb30d6831f2160cffe963d6f5e245f07953bd
xiaolonggou <79433594+xiaolonggou@users.noreply.github.com>
2021-06-25 11:05:20 +0200

    Update main.go

--- interconv/main.go
+++ interconv/main.go
@@ -1,8 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	/*
+		a is an integer
+		b is an integer
+		13
+	*/
+	result := add(5, 8)
+	fmt.Println(result)
+	//a is an integer
+	//ERROR: Type Mismatch
+	result = add(5, 8.3)
+	fmt.Println(result)
+}
 
-	fmt.Println("hello")
+func add(a interface{}, b interface{}) interface{} {
+	var c interface{}
+
+	switch a.(type) {
+	case int:
+		fmt.Println("a is an integer")
+		switch b.(type) {
+		case int:
+			fmt.Println("b is an integer")
+			c = a.(int) + b.(int)
+		default:
+			return "ERROR: Type Mismatch"
+		}
+	default:
+		return "ERROR: Type Mismatch"
+	}
+	return c.(int)
 }
